bf4ebaa62a9970d266121ae6661940663c020564
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2022-06-29 22:19:17 -0700

    Updated App.js
    changes:
    - removed <Navbar />
    - relocated <Info /> to <Visualizer />

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import './App.css';
 import Visualizer from "./Visualizer/Visualizer";
-import Navbar from "./components/Navbar";
-import Info from "./components/Info";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Visualizer />
-      <Info />
+      
     </div>
   );
 }
